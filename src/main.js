bec6bcee4ab4d9a7d67c886e96d6179bb2f13e4e
Rewan Noufal <rewan.noufal.1998@gmail.com>
2023-12-04 16:17:17 +0200

    add comment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue/dist/vue.esm-bundler'
 import App from './App.vue'
 import router from './router'
-import store from './stores'
+// import store from './stores'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,6 +12,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(i18n)
-app.use(store)
+// app.use(store)
 
 app.mount('#app')
